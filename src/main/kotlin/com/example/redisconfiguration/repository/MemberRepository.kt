46c9b39dff6563fe81a1c034e9461b76134869ba
janghyoseog <bestdevhyo1225@gmail.com>
2022-06-28 18:12:47 +0900

    feat: Cache Stampede 현상을 방지하기 위한 PER 알고리즘 도입

--- src/main/kotlin/com/example/redisconfiguration/repository/MemberRepository.kt
+++ src/main/kotlin/com/example/redisconfiguration/repository/MemberRepository.kt
@@ -6,6 +6,8 @@ import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.springframework.stereotype.Repository
 import java.util.concurrent.TimeUnit
+import kotlin.math.abs
+import kotlin.math.ln
 
 @Repository
 class MemberRepository(
@@ -15,7 +17,7 @@ class MemberRepository(
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     fun <T : Any> set(key: String, value: T, expireTime: Long, timeUnit: TimeUnit) {
-        logger.debug("Redis set key: {}, value: {}", key, value)
+        logger.debug("set key: {}, value: {}", key, value)
 
         redisTemplate.opsForValue().set(key, jacksonObjectMapper().writeValueAsString(value))
         redisTemplate.expire(key, expireTime, timeUnit)
@@ -38,12 +40,41 @@ class MemberRepository(
     }
 
     fun <T> get(key: String, clazz: Class<T>): T? {
+        if (isExpiredKey(key = key)) return null
+
         val value = redisTemplate.opsForValue().get(key)
 
-        logger.debug("Redis get value: {}", value)
+        logger.debug("get value: {}", value)
 
         if (value.isNullOrBlank()) return null
 
         return jacksonObjectMapper().readValue(value, clazz)
+    }
+
+    private fun isExpiredKey(key: String, expireTimeGapMs: Long = 3_000L): Boolean {
+        val remainingExpiryTimeMS: Long = redisTemplate.getExpire(key, TimeUnit.MILLISECONDS)
+
+        logger.debug("remainingExpiryTimeMS: {}", remainingExpiryTimeMS)
+
+        return remainingExpiryTimeMS >= 0
+            && getExpiryTimeBasedOnPER(remainingExpiryTimeMS = remainingExpiryTimeMS, delta = expireTimeGapMs) <= 0.0f
+    }
+
+    /**
+     * [ remainingExpiryTimeMS ]
+     * - 남은 만료 시간
+     * [ delta ]
+     * - 캐시를 다시 계산하기 위한 시간 범위 (단위: MS)
+     * [ beta ]
+     * - 가중치 (기본 값으로 1.0을 사용한다.)
+     * - ex) beta < 1.0 => 조금 더 소극적으로 재 계산한다.
+     * - ex) beta > 1.0 => 조금 더 적극적으로 재 계산한다.
+     * */
+    private fun getExpiryTimeBasedOnPER(remainingExpiryTimeMS: Long, delta: Long, beta: Float = 1.0f): Double {
+        val resultExpiryTime = remainingExpiryTimeMS - abs(delta * beta * ln(Math.random()))
+
+        logger.debug("resultExpiryTime: {}", resultExpiryTime)
+
+        return resultExpiryTime
     }
 }
