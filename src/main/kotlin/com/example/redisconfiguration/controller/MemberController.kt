6a5f9a8cce1b0126436deb9e6d52e1ea1b883579
janghyoseog <bestdevhyo1225@gmail.com>
2022-07-02 16:40:09 +0900

    style: 와일드 카드 제거

--- src/main/kotlin/com/example/redisconfiguration/controller/MemberController.kt
+++ src/main/kotlin/com/example/redisconfiguration/controller/MemberController.kt
@@ -9,7 +9,14 @@ import com.example.redisconfiguration.service.dto.CreateMemberCacheResultDto
 import com.example.redisconfiguration.service.dto.FindMemberCacheResultDto
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/redis")
