4467f734181dc9befb150284e89cbe3ec56be427
janghyoseog <bestdevhyo1225@gmail.com>
2022-07-30 14:58:44 +0900

    feat: logger 추가

--- src/main/kotlin/com/example/redisconfiguration/controller/ApiController.kt
+++ src/main/kotlin/com/example/redisconfiguration/controller/ApiController.kt
@@ -3,6 +3,7 @@ package com.example.redisconfiguration.controller
 import com.example.redisconfiguration.controller.response.SuccessResponse
 import com.example.redisconfiguration.service.ApiService
 import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,8 +15,12 @@ class ApiController(
     private val apiService: ApiService
 ) {
 
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     @GetMapping
     fun call(): ResponseEntity<SuccessResponse<Map<String, Any>>> = runBlocking {
+        logger.info("current thread name: {}", Thread.currentThread().name)
+
         val results: List<Long> = apiService.call()
         ResponseEntity.ok(SuccessResponse(data = mapOf("results" to results)))
     }
